4ac205bfee98f85fe9f764cda6244b492ebb5437
Adrián Jesús Peña Rodríguez <adrian.j.p.r@gmail.com>
2018-11-14 14:44:19 +0100

    Finish

--- clicker.py
+++ clicker.py
@@ -1,17 +1,19 @@
-from pymouse import PyMouse
+from pymouse import PyMouse, PyMouseEvent
 import pyautogui as gui
 import time
 import sys
 
-class clickerBot:
+class clickerBot(PyMouseEvent):
     def __init__(self, t_clicks):
         self.mouse = PyMouse()
         gui.PAUSE = float(t_clicks)
         self.__setPositionForClick()
-    
+
     def __setPositionForClick(self):
+        key = 'n'
+        while key != '':
+            key = input()
         self.coords = self.mouse.position()
-        print(self.mouse.position())
     
     def __detectKeyStroke(self):
         if self.mouse.position() != self.coords:
@@ -27,7 +29,7 @@ class clickerBot:
 def main(argv):
     print("Time beetween clicks(s):")
     t_clicks = input()
-    print("Click where you want :)")
+    print("Move the arrow where where you want, then press ENTER :)")
     clicky = clickerBot(t_clicks)
     clicky.start()
     print("Stopped")
