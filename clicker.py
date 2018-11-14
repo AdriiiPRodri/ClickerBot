970942f8c0ef376ec2edb5f51be43459206f8af2
Adrián Jesús Peña Rodríguez <adrian.j.p.r@gmail.com>
2018-11-14 13:19:51 +0100

    Finish

--- clicker.py
+++ clicker.py
@@ -1,56 +1,36 @@
-import platform
+from pymouse import PyMouse
 import pyautogui as gui
-import numpy as np
-import sys
 import time
+import sys
 
+class clickerBot:
+    def __init__(self, t_clicks):
+        self.mouse = PyMouse()
+        gui.PAUSE = float(t_clicks)
+        self.__setPositionForClick()
+    
+    def __setPositionForClick(self):
+        self.coords = self.mouse.position()
+        print(self.mouse.position())
+    
+    def __detectKeyStroke(self):
+        if self.mouse.position() != self.coords:
+            return 1
+        return 0
 
-################### In progress ###################
-
-
-if platform.system() == 'Linux':
-    import pyscreenshot as ImageGrab
-    from PIL import ImageOps
-else:
-    from PIL import ImageGrab, ImageOps
-
-class botDinosaur:
-    def __init__(self):
-        self.replayBtn = (180, 600)
-        self.topRightDino = (255, 379)
-        self.xDistanceToObstacleCloser = 325  # Diferencia aplicada
-        self.yDistanceToObstacleCloser = 369  # Diferencia aplicada
-        self.xDistanceToObstacle = 378  # Diferencia aplicada
-        self.yDistanceToObstacle = 404  # Diferencia aplicada
-
-    def restartGame(self):
-        gui.click(x = self.replayBtn[0], y = self.replayBtn[1])
-
-    def dinoJump(self):
-        gui.keyDown(key = 'space')
-        gui.keyUp(key = 'space')
-
-    def obstacleDetection(self):
-        bbox = (self.xDistanceToObstacleCloser, 
-                self.yDistanceToObstacleCloser,
-                self.xDistanceToObstacle,
-                self.yDistanceToObstacle)
-
-        anilizeRGB = ImageGrab.grab(bbox = bbox)
-        analizeGray = ImageOps.grayscale(anilizeRGB) # Analysing images in grayscale is more effecient than in RGB
-        grey_array = np.array(analizeGray.getcolors()) # Check if the sum of pixel is grey or not
-        return grey_array.sum()
+    def start(self):
+        while self.__detectKeyStroke() != 1:
+            gui.click(x = self.coords[0], y = self.coords[1])
+    
+        
 
 def main(argv):
-    gui.PAUSE = 0.02
-    dino = botDinosaur()
-    contador = 0
-    while(1):
-        contador += 1
-        dino.restartGame()
-        if contador == 700:
-            time.sleep(5)
-            contador = 0
+    print("Time beetween clicks(s):")
+    t_clicks = input()
+    print("Click where you want :)")
+    clicky = clickerBot(t_clicks)
+    clicky.start()
+    print("Stopped")
         
 if __name__ == "__main__":
     main(sys.argv[1:])
